d020bbcabfe843e9797a1fcc397513d228cfb026
Alex_Khomchenko <khomchenkoalex79@gmail.com>
2020-01-02 23:25:01 +0200

    Initial commit

---
+++ src/Number.kt
@@ -0,0 +1,18 @@
+class Number(var intArray: IntArray) {
+    fun getSum(): Int = this.intArray.sum()
+    fun getSum(string: String): String = string.plus(this.getSum())
+    fun getSum(number: Int): Int = number + (this.getSum())
+}
+
+fun main() {
+    val arraySize = (0..10).random()
+    val randomIntArray = IntArray(arraySize) { (0..20).random() }
+
+    Number(randomIntArray).run {
+        this.intArray.forEach { print("$it ") }
+        println()
+        println(this.getSum())
+        println(this.getSum("Sum: "))
+        print(this.getSum(10))
+    }
+}
